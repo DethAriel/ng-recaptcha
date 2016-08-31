1f611e63ff57ca17bdf97cdff0d0ec6f7326bcb9
Ruslan Arkhipau <dethariel@gmail.com>
2016-08-31 13:42:41 -0700

    NgModule support
    * Add a `RecaptchaModule` which encapsulates all the required components
    * Make "forms" version of Recaptcha the default one

--- ng2-recaptcha.ts
+++ ng2-recaptcha.ts
@@ -1,2 +1,4 @@
 export * from './recaptcha/recaptcha.component';
 export * from './recaptcha/recaptcha-loader.service';
+export * from './recaptcha/recaptcha-value-accessor.directive';
+export * from './recaptcha/recaptcha.module';
