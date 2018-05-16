0314d59d4a061a4454c568cc32fc31b486762986
Ruslan Arkhipau <dethariel@gmail.com>
2018-05-16 16:43:39 -0700

    chore(demo): move some demo webpack configuration files around

--- demo/webpack/context.ts
+++
@@ -1,3 +0,0 @@
-import * as path from 'path';
-
-export const context = path.join(process.cwd(), 'src');
--- demo/webpack/output.ts
+++
@@ -1,6 +0,0 @@
-import * as path from 'path';
-
-export const output = {
-  path: path.join(process.cwd(), 'dist', 'ng-recaptcha'),
-  filename: '[name].[chunkhash:8].js',
-};
