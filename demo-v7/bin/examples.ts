9b673fe6f5c9bd28334f3da34af25d1154674dbe
Ruslan Arkhipau <dethariel@gmail.com>
2019-05-31 10:44:44 -0700

    docs(demo): add demo sources for Angular 7

---
+++ demo-v7/bin/examples.ts
@@ -0,0 +1,73 @@
+export interface Example {
+  entry: string;
+  name: string;
+  path: string;
+  label: string;
+  title: string;
+  index?: boolean;
+  additional?: {
+    filename: string,
+    entry: string,
+    title: string,
+    type: string,
+  };
+}
+
+export const examples: Example[] = [
+  {
+    entry: 'demo-basic',
+    name: 'basic',
+    path: '/',
+    label: 'Basic',
+    title: 'Basic Example',
+    index: true,
+  },
+  {
+    entry: 'demo-forms',
+    name: 'forms',
+    path: '/forms',
+    label: 'Forms',
+    title: 'Forms Example',
+  },
+  {
+    entry: 'demo-global-config',
+    name: 'global-config',
+    path: '/global-config',
+    label: 'Global Config',
+    title: 'Global Config Example',
+  },
+  {
+    entry: 'demo-invisible',
+    name: 'invisible',
+    path: '/invisible',
+    label: 'Invisible',
+    title: 'Invisible reCAPTCHA API Example',
+  },
+  {
+    entry: 'demo-v3',
+    name: 'v3',
+    path: '/v3',
+    label: 'reCAPTCHA v3',
+    title: 'reCAPTCHA v3 Example',
+  },
+  {
+    entry: 'demo-language',
+    name: 'language',
+    path: '/language',
+    label: 'Language',
+    title: 'Language Example',
+  },
+  {
+    entry: 'demo-preload-api',
+    name: 'preload-api',
+    path: '/preload-api',
+    label: 'Preloaded',
+    title: 'Preloaded reCAPTCHA API Example',
+    additional: {
+      filename: 'preload-api-demo.head',
+      entry: 'head-preload-api',
+      title: 'script.ts',
+      type: 'ts',
+    },
+  },
+];
