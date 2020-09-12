5cc5fc36b93daa5abac41c43856c86413d152654
Ruslan Arkhipau <dethariel@gmail.com>
2020-09-11 22:20:32 -0700

    chore(docs): update v3 examples to show how to catcha recaptcha errors

--- demo/v-all/src/app/examples/v3/v3-demo.component.ts
+++ demo/v-all/src/app/examples/v3/v3-demo.component.ts
@@ -1,17 +1,22 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { OnExecuteData, ReCaptchaV3Service } from 'ng-recaptcha';
+import { OnExecuteData, OnExecuteErrorData, ReCaptchaV3Service } from 'ng-recaptcha';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'recaptcha-demo',
+  styles: [`
+    .error { color: crimson; }
+` ],
   templateUrl: './v3-demo.component.html',
 })
 export class RecaptchaV3DemoComponent implements OnInit, OnDestroy {
   public recentToken: string = '';
-  public readonly executionLog: OnExecuteData[] = [];
+  public recentError?: { error: any };
+  public readonly executionLog: Array<OnExecuteData | OnExecuteErrorData> = [];
 
   private allExecutionsSubscription: Subscription;
+  private allExecutionErrorsSubscription: Subscription;
   private singleExecutionSubscription: Subscription;
 
   constructor(
@@ -24,17 +29,31 @@ export class RecaptchaV3DemoComponent implements OnInit, OnDestroy {
       this.singleExecutionSubscription.unsubscribe();
     }
     this.singleExecutionSubscription = this.recaptchaV3Service.execute(action)
-      .subscribe((token) => this.recentToken = token);
+      .subscribe(
+        (token) => {
+          this.recentToken = token;
+          this.recentError = undefined;
+        },
+        (error) => {
+          this.recentToken = '';
+          this.recentError = { error };
+        },
+      );
   }
 
   public ngOnInit() {
     this.allExecutionsSubscription = this.recaptchaV3Service.onExecute
+      .subscribe((data) => this.executionLog.push(data));
+    this.allExecutionErrorsSubscription = this.recaptchaV3Service.onExecuteError
       .subscribe((data) => this.executionLog.push(data));
   }
 
   public ngOnDestroy() {
     if (this.allExecutionsSubscription) {
       this.allExecutionsSubscription.unsubscribe();
+    }
+    if (this.allExecutionErrorsSubscription) {
+      this.allExecutionErrorsSubscription.unsubscribe();
     }
     if (this.singleExecutionSubscription) {
       this.singleExecutionSubscription.unsubscribe();
