84a3bfe5322d8ea8f5320e089dfcaba5061ff3a2
Luc Fauvel <lfauvel@devolutions.net>
2022-04-25 13:21:14 -0400

    fix: Made language an observable that can update the language of the iframe in real time, also changed to publish with jfrog

--- projects/ng-recaptcha/src/lib/load-script.ts
+++ projects/ng-recaptcha/src/lib/load-script.ts
@@ -4,27 +4,35 @@ declare global {
   }
 }
 
+let script: HTMLScriptElement;
+
 function loadScript(
   renderMode: "explicit" | string,
   onLoaded: (grecaptcha: ReCaptchaV2.ReCaptcha) => void,
   urlParams: string,
+  language: string,
   url?: string,
   nonce?: string
 ): void {
   window.ng2recaptchaloaded = () => {
     onLoaded(grecaptcha);
   };
-  const script = document.createElement("script");
-  script.innerHTML = "";
-  const baseUrl = url || "https://www.google.com/recaptcha/api.js";
+  if (!script) {
+    script = document.createElement("script");
+    script.innerHTML = "";
+    const baseUrl = url || "https://www.google.com/recaptcha/api.js";
 
-  script.src = `${baseUrl}?render=${renderMode}&onload=ng2recaptchaloaded${urlParams}`;
-  if (nonce) {
-    script.nonce = nonce;
+    script.src = `${baseUrl}?render=${renderMode}&onload=ng2recaptchaloaded${urlParams}`;
+    if (nonce) {
+      script.nonce = nonce;
+    }
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  } else {
+    const iframe = document.querySelector('[title="reCAPTCHA"]');
+    iframe.setAttribute("src", iframe.getAttribute("src").replace(/hl=(.*?)&/, "hl=" + language + "&"));
   }
-  script.async = true;
-  script.defer = true;
-  document.head.appendChild(script);
 }
 
 export const loader = { loadScript };
